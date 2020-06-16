0180e4a4d6d60433098fd9429d32c1f0bb58c3bd
Bakumegusta <bakumegusta@gmail.com>
2020-06-16 15:57:26 +0530

    update

--- main.js
+++ main.js
@@ -1,3 +1,30 @@
 $('.nav-item').on('click', () => {
     $('.home').removeClass('active');
 })
+
+$('.nav-item').on('click', (e) => {
+    console.log(e.target.id);
+    var id = e.target.id;
+    if (id == 'me') {
+        $('#aboutme').show();
+        $('#hobbies').hide();
+        $('#contact').hide();
+    } else if (id == 'mehobbies') {
+        $('#hobbies').show();
+        $('#contact').hide();
+        $('#aboutme').hide();
+    } else if (id == 'mecontact') {
+        $('#contact').show();
+        $('#aboutme').hide();
+        $('#hobbies').hide();
+    } else {
+        $('#aboutme').hide();
+        $('#hobbies').hide();
+        $('#contact').hide();
+    }
+})
+
+
+$('.snip1582').on('click', () => {
+    $('.aboutme').hide();
+})
