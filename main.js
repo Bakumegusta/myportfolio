b5c2c9704dc9ea9a1081bcdd7ee088d258a5ffbe
Bakumegusta <bakumegusta@gmail.com>
2020-06-18 17:06:52 +0530

    update

--- main.js
+++ main.js
@@ -1,3 +1,33 @@
+// onload animation
+window.sr = ScrollReveal();
+sr.reveal('.header', {
+  duration: 2000,
+  origin:'top',
+  distance:'300px'
+
+});
+sr.reveal('.dataleft', {
+  duration: 2000,
+  origin:'left',
+  delay: 1000,
+  distance:'300px'
+});
+sr.reveal('.dataright', {
+  duration: 2000,
+  delay: 2000,
+  origin:'right',
+  distance:'300px'
+});
+sr.reveal('.widgeticon', {
+    duration: 2000,
+    delay: 2000,
+    origin:'bottom',
+    distance:'300px'
+  });
+// animaton end
+
+
+
 $('.nav-item').on('click', () => {
     $('.home').removeClass('active');
 })
